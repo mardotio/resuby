6c07d90479d52795800f9a021329cca99f544054
Mario Lopez <e.miliozx.er@gmail.com>
2018-09-09 17:24:10 -0400

    Improved resuby exe (#3)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,18 @@
 # Change log
 
-All notable changes to this project will be documented in this file. The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
-and this project adheres to [Semantic Versioning](http://semver.org).
+All notable changes to this project will be documented in this file. The format is based on [Keep a
+Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org).
 
-## Release 0.0.2
+## Release 0.1.0
 ### Summary
 
-General bug fixes and enhancements
+Add features to `resuby` executable
+
+#### Added
+- `resuby` executable now has more options
+- Converted Resuby into a module
+- `Resuby::Resume` class
+- `Resuby::CLI` module
 
 #### Fixed
 - Trailing whitelines in generated HTML file
---
+++ lib/resuby/resume/resume.rb
@@ -0,0 +1,45 @@
+require 'erb'
+require 'yaml'
+require 'sass'
+require 'resuby/version'
+
+class Resuby::Resume
+  attr_reader :name, :contact_info, :sections, :template, :profile
+
+  def initialize
+    @template = File.read(File.join(__dir__, '../templates/resume.erb'))
+  end
+
+  def read_source(source)
+    puts "Reading in data from #{source}"
+    resume_data = YAML.load_file(source)
+    @name = resume_data['name']
+    @contact_info = resume_data['contact']
+    @profile = resume_data['profile']
+    @sections = resume_data.reject { |key, value| ['name', 'contact', 'profile'].include?(key) }
+  end
+
+  def render
+    ERB.new(self.template, nil, '-').result( binding )
+  end
+
+  def save_resume(file)
+    puts "Saving your resume at #{file}"
+    File.open(file, "w+") do |f|
+      f.write(render)
+    end
+  end
+
+  def compile_styles
+    options = {
+      cache: true,
+      syntax: :scss,
+      style: :compressed,
+    }
+
+    scss_file = File.join(__dir__, '../css/main.scss')
+    render = Sass::Engine.new(File.read(scss_file), options).render
+    Dir.mkdir(File.join(Dir.pwd, 'css'), 0755) unless File.directory?(File.join(Dir.pwd, 'css'))
+    File.write(File.join(Dir.pwd, 'css/main.css'), render)
+  end
+end
